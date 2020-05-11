c547905135f83fdd499366744b3a5b52878504cc
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-11 19:34:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Your code here
 
 function mapToNegativize(array) {
+  let newArray = [];
   
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(-array[i]);
+  }
+  
+  return newArray;
 }
